6d3efab81cfcb782d8cae1338d1171cb2c1e21cf
johnnoDev <johnny.lopezvera20@gmail.com>
2026-06-07 18:50:46 -0500

    rutas dinamicas

--- my-project/customer/urls.py
+++ my-project/customer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio),
+    path('<dia>', views.dia_semana)  # <dia> captura cualquier texto de la URL y lo pasa como parámetro a la vista
 ]
--- my-project/customer/views.py
+++ my-project/customer/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def inicio(request):
     return HttpResponse('Hola mundo desde Inicio')
+
+# Diccionario que mapea cada día de la semana a su frase
+frase_dia = {
+    'lunes': 'Hoy es lunes',
+    'martes': 'Hoy es martes',
+    'miercoles': 'Hoy es miercoles',
+    'jueves': 'Hoy es jueves',
+    'viernes': 'Hoy es viernes',
+    'sabado': 'Hoy es sabado',
+    'domingo': 'Hoy es domingo'
+}
+
+# 'dia' llega como parámetro dinámico desde la URL (ej: /lunes → dia='lunes')
+def dia_semana(request, dia):
+    texto_por_dia = frase_dia[dia]
+    return HttpResponse(texto_por_dia)
