46f5bf4de0b856798faf3767a79552911886d292
johnnoDev <johnny.lopezvera20@gmail.com>
2026-06-07 19:14:57 -0500

    redirecciones y mas

--- my-project/customer/urls.py
+++ my-project/customer/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio),
-    path('<dia>', views.dia_semana)  # <dia> captura cualquier texto de la URL y lo pasa como parámetro a la vista
+    path('<int:dia>', views.dia_semana_numero),
+    path('<str:dia>', views.dia_semana)  # <dia> captura cualquier texto de la URL y lo pasa como parámetro a la vista
+
 ]
--- my-project/customer/views.py
+++ my-project/customer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 def inicio(request):
     return HttpResponse('Hola mundo desde Inicio')
@@ -14,6 +14,15 @@ frase_dia = {
     'sabado': 'Hoy es sabado',
     'domingo': 'Hoy es domingo'
 }
+
+def dia_semana_numero(request, dia):
+    dias = list(frase_dia.keys())  # ['lunes', 'martes', ...]
+    if dia > len(dias):
+        return HttpResponse('El día no existe')
+    dia_rediccionar = dias[dia-1]  # dia=1 → índice 0 → 'lunes'
+    # Redirige al navegador a la ruta con el nombre del día (ej: /lunes)
+    return HttpResponseRedirect(f'/{dia_rediccionar}')
+
 
 # 'dia' llega como parámetro dinámico desde la URL (ej: /lunes → dia='lunes')
 def dia_semana(request, dia):
